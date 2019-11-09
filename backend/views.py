5bac3587c7d062df5fb6f0ae909945e889a534c0
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-11-09 23:31:27 +0300

    Fix error by giving names to endpoints

--- backend/views.py
+++ backend/views.py
@@ -7,7 +7,7 @@ conn = psycopg2.connect(host='localhost', user='postgres',
                         password='password', database='hospital')
 
 
-@api.route('/doctors')
+@api.route('/doctors', endpoint='doctors')
 def api_home():
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     cur.execute('SELECT * FROM doctors')
@@ -16,7 +16,7 @@ def api_home():
     return jsonify(results)
 
 
-@api.route('/patients')
+@api.route('/patients', endpoint='patients')
 def api_home():
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     cur.execute('SELECT * FROM patients')
@@ -25,7 +25,7 @@ def api_home():
     return jsonify(results)
 
 
-@api.route('/nurses')
+@api.route('/nurses', endpoint='nurses')
 def api_home():
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     cur.execute('SELECT * FROM nurses')
