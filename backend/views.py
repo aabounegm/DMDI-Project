51d1981b8868a595c8575e6ff61c6b93e7f7e0ca
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-11-24 01:48:48 +0300

    Add endpoint for 4th query

--- backend/views.py
+++ backend/views.py
@@ -110,6 +110,57 @@ def api_home():
     return jsonify(results)
 
 
+@api.route('/query4', endpoint='query4')
+def api_home():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute('''SELECT SUM
+                    (
+                    CASE
+                        WHEN (Slots_2.patient_age >= 50) THEN
+                            CASE
+                                WHEN (Slots_2.appointments >=3) THEN
+                                    500*Slots_2.appointments
+                                ELSE
+                                    400*Slots_2.appointments
+                            END
+                        ELSE
+                            CASE
+                                WHEN (Slots_2.appointments >=3) THEN 250*Slots_2.appointments
+                                ELSE 200*Slots_2.appointments
+                            END
+                    END
+                    )
+                    FROM
+                    (
+                    SELECT
+                        Slots.patient_id,
+                        Slots.patient_age,
+                        COUNT(*) as appointments
+                    FROM
+                    (
+                        SELECT
+                            Patients.id AS patient_id,
+                            (EXTRACT (YEARS FROM NOW())) -
+                            (EXTRACT( YEARS FROM Patients.date_of_birth))
+                                as patient_age
+                        FROM
+                            Patients, Appointments
+                            WHERE
+                                Appointments.patient_id = Patients.id AND
+                                (Appointments.DATE >= NOW() - INTERVAL'1 Month') AND
+                                (Appointments.DATE <= NOW())
+                    ) AS Slots
+                    GROUP BY
+                        Slots.patient_id,
+                        Slots.patient_age
+                    ) AS Slots_2;'''
+                )
+    result = cur.fetchone()
+    result['sum'] = float(result['sum'] or 0)
+    cur.close()
+    return jsonify(result)
+
+
 @api.route('/doctors/query5', endpoint='query5')
 def api_home():
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
